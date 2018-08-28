b4f3624d718008226b810e59a91569a0ba4abe12
sathwik <www.msathwik11@gmail.com>
2018-08-28 08:52:10 +0530

    Assignment1 - Android App

---
+++ Assignments/HelpingHands/app/src/main/java/com/example/sandy/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.sandy.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void signIn(View view){
+        Intent intent = new Intent(this,SignIn.class);
+        startActivity(intent);
+    }
+    public void signUpForm(View view){
+        Intent intent = new Intent(this,SignUpForm.class);
+        startActivity(intent);
+    }
+}
